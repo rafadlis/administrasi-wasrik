8799d12c25690e36bbbb7c4fe07958be48293fde
Rafadlis <rahmatfadlis@gmail.com>
2024-09-09 09:23:17 +0700

    refactor: redesign the ui

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,23 @@ import { columnsPelaksanaan } from "@/components/columns-pelaksanaan";
 import { DataTable } from "@/components/data-tables";
 import { getDaftarKegiatanPemeriksaan } from "@/lib/get-kegiatan";
 import { NewKegiatanDialog } from "@/components/new-kegiatan-form";
+// import ProgressValueCard from "@/components/progress-value-card";
+// import { BarChartMultipleCard } from "@/components/bar-chart-multiple";
+// import { BarChartMultiple } from "@/components/bar-chart-multiple";
+
 export default async function Home() {
   const data = await getDaftarKegiatanPemeriksaan();
   return (
-    <main className="w-full py-3">
-      <section></section>
-      <section className="max-w-screen-xl mx-auto flex flex-col gap-3">
-        <NewKegiatanDialog />
+    <main className="flex flex-col max-w-screen-xl mx-auto w-full py-3 gap-3">
+      <section className="flex flex-row gap-3">
+        {/* <BarChartMultipleCard
+          title={""}
+          description={""}
+          chartConfig={undefined}
+        /> */}
+      </section>
+      <section className="flex flex-col gap-3">
+        <NewKegiatanDialog className="self-start ml-auto" />
         <DataTable columns={columnsPelaksanaan} data={data} />
       </section>
     </main>
