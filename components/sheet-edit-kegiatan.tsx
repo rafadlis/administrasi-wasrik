6ab829fecbc9fbbf8b68e2ba4ed8704eb4e93853
Rafadlis <rahmatfadlis@gmail.com>
2024-09-11 16:24:32 +0700

    feat: add new fields for edit

--- components/sheet-edit-kegiatan.tsx
+++ components/sheet-edit-kegiatan.tsx
@@ -25,11 +25,21 @@ import { DaftarKegiatanPemeriksaanType } from "@/lib/get-kegiatan";
 import {
   useHasilPemeriksaan,
   useJenisPemeriksaan,
-  useProgressPemeriksaan,
   useTim,
 } from "@/lib/get-other-client";
 import { updateKegiatanPemeriksaan } from "@/lib/update-kegiatan";
 import { toast } from "sonner";
+import { Separator } from "./ui/separator";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "./ui/table";
+import { Input } from "./ui/input";
 
 export function SheetEditKegiatan({
   data,
@@ -39,8 +49,6 @@ export function SheetEditKegiatan({
   // MARK: Data
   const { dataJenisPemeriksaan, isLoadingJenisPemeriksaan } =
     useJenisPemeriksaan();
-  const { dataProgressPemeriksaan, isLoadingProgressPemeriksaan } =
-    useProgressPemeriksaan();
   const { dataHasilPemeriksaan, isLoadingHasilPemeriksaan } =
     useHasilPemeriksaan();
   const { dataTim, isLoadingTim } = useTim();
@@ -49,22 +57,6 @@ export function SheetEditKegiatan({
   const handleJenisPemeriksaanChange = async (value: string) => {
     await updateKegiatanPemeriksaan(data.id, {
       jenis_pemeriksaan_id: parseInt(value),
-    }).then((res) => {
-      if (res.type === "success") {
-        toast.success(res.header, {
-          description: res.message,
-        });
-      } else {
-        toast.error(res.header, {
-          description: res.message,
-        });
-      }
-    });
-  };
-
-  const handleProgressChange = async (value: string) => {
-    await updateKegiatanPemeriksaan(data.id, {
-      progress_id: parseInt(value),
     }).then((res) => {
       if (res.type === "success") {
         toast.success(res.header, {
@@ -110,6 +102,38 @@ export function SheetEditKegiatan({
     });
   };
 
+  const handleTanggalAwalChange = async (value: string) => {
+    await updateKegiatanPemeriksaan(data.id, {
+      tgl_pemeriksaan_mulai: new Date(value),
+    }).then((res) => {
+      if (res.type === "success") {
+        toast.success(res.header, {
+          description: res.message,
+        });
+      } else {
+        toast.error(res.header, {
+          description: res.message,
+        });
+      }
+    });
+  };
+
+  const handleTanggalAkhirChange = async (value: string) => {
+    await updateKegiatanPemeriksaan(data.id, {
+      tgl_pemeriksaan_selesai: new Date(value),
+    }).then((res) => {
+      if (res.type === "success") {
+        toast.success(res.header, {
+          description: res.message,
+        });
+      } else {
+        toast.error(res.header, {
+          description: res.message,
+        });
+      }
+    });
+  };
+
   return (
     <Sheet>
       <SheetTrigger asChild>
@@ -128,37 +152,34 @@ export function SheetEditKegiatan({
         <Tabs className="mt-6" defaultValue="kegiatan">
           <TabsList>
             <TabsTrigger value="kegiatan">Kegiatan</TabsTrigger>
+            <TabsTrigger value="tim">Tim</TabsTrigger>
             <TabsTrigger value="wp">WP</TabsTrigger>
-            <TabsTrigger value="output">Output</TabsTrigger>
-            <TabsTrigger value="dokumen">Dokumen</TabsTrigger>
+            <TabsTrigger value="hasil">Hasil</TabsTrigger>
+            <TabsTrigger value="dokumen">Dok</TabsTrigger>
           </TabsList>
-
+          {/* MARK: Kegiatan */}
           <TabsContent value="kegiatan" className="flex flex-col gap-4 mt-6 ">
             <fieldset className="flex flex-col gap-2">
-              <Label>Tim</Label>
-              <Select
-                value={data.tim?.id?.toString() ?? ""}
-                onValueChange={handleTimChange}
-              >
-                <SelectTrigger disabled={isLoadingTim}>
-                  <SelectValue
-                    placeholder={
-                      isLoadingTim
-                        ? "Memuat..."
-                        : data.tim?.nama
-                        ? data.tim?.nama
-                        : "Pilih..."
-                    }
-                  />
-                </SelectTrigger>
-                <SelectContent>
-                  {dataTim.map((tim) => (
-                    <SelectItem key={tim.id} value={tim.id.toString()}>
-                      {tim.nama}
-                    </SelectItem>
-                  ))}
-                </SelectContent>
-              </Select>
+              <Label>Tanggal Awal</Label>
+              <Input
+                type="date"
+                value={
+                  data.tgl_pemeriksaan_mulai
+                    ? data.tgl_pemeriksaan_mulai.toISOString().split("T")[0]
+                    : ""
+                }
+                onChange={(e) => handleTanggalAwalChange(e.target.value)}
+              />
+              <Label>Tanggal Akhir</Label>
+              <Input
+                type="date"
+                value={
+                  data.tgl_pemeriksaan_selesai
+                    ? data.tgl_pemeriksaan_selesai.toISOString().split("T")[0]
+                    : ""
+                }
+                onChange={(e) => handleTanggalAkhirChange(e.target.value)}
+              />
             </fieldset>
             <fieldset className="flex flex-col gap-2">
               <Label>Jenis Pemeriksaan</Label>
@@ -186,38 +207,57 @@ export function SheetEditKegiatan({
                 </SelectContent>
               </Select>
             </fieldset>
-
+          </TabsContent>
+          {/* MARK: Tim */}
+          <TabsContent value="tim" className="flex flex-col gap-4">
             <fieldset className="flex flex-col gap-2">
-              <Label>Progress</Label>
+              <Label>Tim</Label>
               <Select
-                value={data.progress?.id?.toString() ?? ""}
-                onValueChange={handleProgressChange}
+                value={data.tim?.id?.toString() ?? ""}
+                onValueChange={handleTimChange}
               >
-                <SelectTrigger disabled={isLoadingProgressPemeriksaan}>
+                <SelectTrigger disabled={isLoadingTim}>
                   <SelectValue
                     placeholder={
-                      isLoadingProgressPemeriksaan
+                      isLoadingTim
                         ? "Memuat..."
-                        : data.progress?.nama
-                        ? data.progress?.nama
+                        : data.tim?.nama
+                        ? data.tim?.nama
                         : "Pilih..."
                     }
                   />
                 </SelectTrigger>
                 <SelectContent>
-                  {dataProgressPemeriksaan.map((progress) => (
-                    <SelectItem
-                      key={progress.id}
-                      value={progress.id.toString()}
-                    >
-                      {progress.nama}
+                  {dataTim.map((tim) => (
+                    <SelectItem key={tim.id} value={tim.id.toString()}>
+                      {tim.nama}
                     </SelectItem>
                   ))}
                 </SelectContent>
               </Select>
+              <Separator className="my-2" />
+              <Table>
+                <TableHeader>
+                  <TableRow>
+                    <TableHead>Nama</TableHead>
+                    <TableHead>Jabatan</TableHead>
+                  </TableRow>
+                </TableHeader>
+                <TableBody>
+                  {data.tim?.anggota_tim.map((anggota) => (
+                    <TableRow key={anggota.petugas.id}>
+                      <TableCell>{anggota.petugas.panggilan}</TableCell>
+                      <TableCell>{anggota.petugas.jabatan}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+                <TableCaption>Anggota Tim</TableCaption>
+              </Table>
+              <Button className="mt-3 ml-auto">+ Tim Baru</Button>
             </fieldset>
           </TabsContent>
-          <TabsContent value="output" className="flex flex-col gap-4">
+          {/* MARK: Hasil */}
+          <TabsContent value="hasil" className="flex flex-col gap-4">
             <fieldset className="flex flex-col gap-2">
               <Label>Hasil Pemeriksaan</Label>
               <Select
